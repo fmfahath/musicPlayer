2381d87610fb948a595064086fde938d15b89312
fmfahath <fmfahath.dev@gmail.com>
2023-12-18 02:28:54 +0530

    slider function to play/pause the song added++

--- app.js
+++ app.js
@@ -10,15 +10,30 @@ songEl.onloadedmetadata = function () {
     // console.log(progressEl.value);
 }
 
-function playPause(){
-    if(ctrlEl.classList.contains('fa-pause')){
+//play or pause
+function playPause() {
+    if (ctrlEl.classList.contains('fa-pause')) {
         songEl.pause();
         ctrlEl.classList.remove('fa-pause');
         ctrlEl.classList.add('fa-play');
     }
-    else{
+    else {
         songEl.play();
         ctrlEl.classList.remove('fa-play');
         ctrlEl.classList.add('fa-pause');
     }
+}
+
+//moving to play / pause song
+if (songEl.play()) {
+    setInterval(() => {
+        progressEl.value = songEl.currentTime;
+    }, 500);
+}
+
+progressEl.onchange = function () {
+    songEl.play();
+    songEl.currentTime = progressEl.value;
+    ctrlEl.classList.remove('fa-play');
+    ctrlEl.classList.add('fa-pause');
 }
