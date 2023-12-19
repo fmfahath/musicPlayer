1623946fac4a65c10e2b8b3126b37ce3ec4c09d7
fmfahath <fmfahath.dev@gmail.com>
2023-12-19 15:50:18 +0530

    moifoed

--- app.js
+++ app.js
@@ -1,39 +1,43 @@
-let songEl = document.getElementById('song');
-let progressEl = document.getElementById('progress');
-let ctrlEl = document.getElementById('ctrlIcon');
+const imgEl = document.getElementById('img');
+const songEl = document.getElementById('song');
+const progressEl = document.getElementById('progress');
+const playPauseBtnEl = document.getElementById('playPause-btn');
 
-songEl.onloadedmetadata = function () {
+songEl.onloadedmetadata = function(){
     progressEl.max = songEl.duration;
     progressEl.value = songEl.currentTime;
 
-    // console.log(progressEl.max);
-    // console.log(progressEl.value);
-}
+    // console.log(progressEl.max, progressEl.value)
+};
 
 //play or pause
-function playPause() {
-    if (ctrlEl.classList.contains('fa-pause')) {
-        songEl.pause();
-        ctrlEl.classList.remove('fa-pause');
-        ctrlEl.classList.add('fa-play');
+function playPause(){
+    if(playPauseBtnEl.classList.contains('fa-pause')){
+          songEl.pause();
+          playPauseBtnEl.classList.remove('fa-pause');
+          imgEl.classList.remove('play-mode');
+          playPauseBtnEl.classList.add('fa-play');
     }
-    else {
-        songEl.play();
-        ctrlEl.classList.remove('fa-play');
-        ctrlEl.classList.add('fa-pause');
+    else{
+      songEl.play();
+      playPauseBtnEl.classList.remove('fa-play');
+      playPauseBtnEl.classList.add('fa-pause');
+      imgEl.classList.add('play-mode');
+     
     }
-}
+  }
 
 //moving to play / pause song
-if (songEl.play()) {
+if(songEl.play()){
     setInterval(() => {
-        progressEl.value = songEl.currentTime;
+      progressEl.value = songEl.currentTime;
     }, 500);
-}
+  }
 
-progressEl.onchange = function () {
+  progressEl.onchange = function(){
     songEl.play();
-    songEl.currentTime = progressEl.value;
-    ctrlEl.classList.remove('fa-play');
-    ctrlEl.classList.add('fa-pause');
-}
+    songEl.currentTime = progressEl.value
+    playPauseBtnEl.classList.remove('fa-play');
+    playPauseBtnEl.classList.add('fa-pause');
+  
+  }
